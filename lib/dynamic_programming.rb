3f8a5344fe008091c7a83ae02a65b0f389c8e91c
Alex <alexanderhapp@me.com>
2018-08-23 18:12:59 -0700

    Fix typo

--- lib/dynamic_programming.rb
+++ lib/dynamic_programming.rb
@@ -4,6 +4,7 @@ require 'byebug'
 class DynamicProgramming
   def initialize
     @blair_cache = []
+    @frog_cache = { 1 => [[1]], 2 => [[1,1], [2]], 3 => [[1,2], [2,1], [1,1,1], [3]] }
   end
 
   def blair_nums(n)
@@ -30,7 +31,7 @@ class DynamicProgramming
 
   def frog_cache_builder(n)
     cache = { 1 => [[1]], 2 => [[1,1], [2]], 3 => [[1,2], [2,1], [1,1,1], [3]] }
-    return cache if n < 4
+    return cache if n <= 3
     4.upto(n) do |i|
       solutions = []
       3.downto(1) do |j|
@@ -46,11 +47,25 @@ class DynamicProgramming
   end
 
   def frog_hops_top_down(n)
-
+    ans = frog_hops_top_down_helper(n)
+    ans
   end
 
   def frog_hops_top_down_helper(n)
-
+    if !@frog_cache[n].nil?
+      ans = @frog_cache[n]
+      return ans
+    else 
+      return @frog_cache[n] if n < 4
+      ans = []
+      3.downto(1) do |i|
+        prev = frog_hops_top_down_helper(n - i)
+        new_prev = prev.dup.map {|arr| arr << i}
+        ans << new_prev
+      end 
+      @frog_cache[n] = ans
+      ans
+    end 
   end
 
   def super_frog_hops(n, k)
